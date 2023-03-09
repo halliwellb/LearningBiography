d4a0fefa06f0ddfc5e04522b8ee48344db7c315b
Ben Halliwell <bkhalliwell@outlook.com>
2023-03-09 21:51:10 +0000

    modified:   app.js

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 'use strict';
 
+function askQuestion(questions, answers) {
+  let response = prompt(questions);
+  if (response) {
+    if (response === answers) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
 let questionOne = 'I would like to ask you a few questions. Would that be alright?';
 let questionTwo = 'The first question is: Do I live in Maine?';
 let questionThree = 'The second question is: Do I have a finance degree?';
@@ -10,7 +21,7 @@ let questionEight = 'The sixth question is: Am I training in software developmen
 
 let questions = [questionOne, questionTwo, questionThree, questionFour, questionFive, questionSix, questionEight];
 let answers = ['yes', 'yes', 'yes', 'no', 'yes', 'yes', 'yes'];
-let response = null;
+let response = [''];
 
 let name = prompt('Hello! What is your name?');
 alert('Hi, ' + name + '! Thanks for coming by!');
@@ -18,33 +29,31 @@ alert('Hi, ' + name + '! Thanks for coming by!');
 let r = 0;
 
 for (let i = 0; i < questions.length; i++) {
-  {
-    response = prompt(questions[i]);
-    console.log(questions);
-    if (response === answers[i]) {
-      alert('Great!'); {
-        r++;
-      }
-    } else {
-      alert('Wrong!');
-    }
+  let userCorrect = askQuestion(questions[i], answers[i]);
+  if (userCorrect) {
+    alert('Awesome!');
+    r++;
+    console.log(userCorrect);
+  } else {
+    alert('Wrong!');
+    console.log(userCorrect);
   }
 }
 
 let myNumber = 17;
 let trys = 0;
-let questionSeven = prompt('Please choose a number between 1-20');
-if (questionSeven === '') {
-  questionSeven = prompt('Please choose a number! 1-20');
+let questionNumberGuess = prompt('Please choose a number between 1-20');
+if (questionNumberGuess === '') {
+  questionNumberGuess = prompt('Please choose a number! 1-20');
 } while (trys < 5) {
-  if (questionSeven == myNumber) {
+  if (questionNumberGuess == myNumber) {
     alert('Wow, that is exactly right!');
     break;
-  } else (questionSeven = prompt('I am sorry, incorrect. Please choose another number'));
+  } else (questionNumberGuess = prompt('I am sorry, incorrect. Please choose another number'));
   trys += 1;
 }
 
 let parting = alert('Thank you for learning a bit about me, ' + name + '. You got ' + r + ' questions correct!');
 console.log(parting);
 
-// document.write('<h4>Great job, ' + name + '! You got ' + r + ' questions correct!</h4>');
+document.write('<h4>Great job, ' + name + '! You got ' + r + ' questions correct!</h4>');
