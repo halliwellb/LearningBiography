fde5791ea19d2464c506d6b8e26b948c2d6d3123
Ben Halliwell <bkhalliwell@outlook.com>
2023-03-09 11:48:56 +0000

    modified:   README.md 	modified:   app.js 	modified:   index.html

--- README.md
+++ README.md
@@ -2,13 +2,20 @@
 
 ## LearningBiography
 
-Asks questions about the owner of the webpage for user to learn
+This project asks questions so the user can learn more about the owner of the webpage. The final question is a "guess a number" sequence between 1-20, offering six trys. HTML, CSS, and JavaScript are used in this project.
 
 ### Author: Ben Halliwell
 
 ### Links and Resources
 
+Line 26 - [Picture credit: B. Halliwell](https://beehoppyflowers.com/)
+
+Line 46 - [Picture credit: Cameron Shepherd; Lewis Pugh Foundation](https://lewispughfoundation.org/)
+
 ### Lighthouse Accessibility Report Score
 
+#### Score for Lab 2
 ![Lighthouse score Lab 2](css/img/website_outcome_Lab2_201.png)
+
+#### Score for Lab 3
 ![Lighthouse score Lab3](css/img/LighthouseScoreLab3.png)
--- app.js
+++ app.js
@@ -15,10 +15,11 @@ let response = null;
 let name = prompt('Hello! What is your name?');
 alert('Hi, ' + name + '! Thanks for coming by!');
 
-let r =0;
+let r = 0;
 
-for (let i =0; i < questions.length; i++) {
-  {response = prompt(questions[i]);
+for (let i = 0; i < questions.length; i++) {
+  {
+    response = prompt(questions[i]);
     console.log(questions);
     if (response === answers[i]) {
       alert('Great!'); {
@@ -35,7 +36,7 @@ let trys = 0;
 let questionSeven = prompt('Please choose a number between 1-20');
 if (questionSeven === '') {
   questionSeven = prompt('Please choose a number! 1-20');
-} while(trys < 5) {
+} while (trys < 5) {
   if (questionSeven == myNumber) {
     alert('Wow, that is exactly right!');
     break;
@@ -47,65 +48,3 @@ let parting = alert('Thank you for learning a bit about me, ' + name + '. You go
 console.log(parting);
 
 document.write('Great job! You got ' + r + ' questions correct!');
-
-// 'use strict';
-
-// let name = prompt('Hello! What is your name?');
-// alert('Hi, ' + name + '! Thanks for coming by! I would like to ask you a few questions.');
-// // console.log('Your name is ' + name + '.');
-
-// let questionOne = prompt('Would that be alright, ' + name + '?').toLowerCase();
-
-// if (questionOne === '') {
-//   questionOne = prompt('Please answer yes or no.');
-// } else if (questionOne === 'yes') {
-//   alert('Awesome! Coming right up.');
-// //   console.log('Thanks for playing, ' + name + '!');
-// } else {
-//   alert('I do not understand..If yes, please type yes.');
-// //   console.log('I do not understand..If yes, please type yes.');
-// }
-
-// let questionTwo = prompt('The first question is: Do I live in Maine?').toLowerCase();
-// if (questionTwo === '') {
-//   questionTwo = prompt('Please answer yes or no.');
-// } else if (questionTwo === 'yes') {
-//   alert('Correct!');
-// //   console.log('Correct, ' + name + '!');
-// } else {
-//   alert('Incorrect, sorry.');
-// //   console.log('Incorrect, ' + name + ' sorry.');
-// }
-
-// let questionThree = prompt('The second question is: Do I have a finance degree?').toLowerCase();
-// if (questionThree === '') {
-//   questionThree = prompt('Please answer yes or no.');
-// } else if (questionThree === 'yes') {
-//   alert('Correct!');
-// //   console.log('Correct, ' + name + '!');
-// } else {
-//   alert('Incorrect, sorry.');
-// //   console.log('Incorrect, ' + name + ' sorry.');
-// }
-
-// let questionFour = prompt('The third question is: Do I still work in financial planning?').toLowerCase();
-// if (questionFour === '') {
-//   questionFour = prompt('Please answer yes or no.');
-// } else if (questionFour === 'no') {
-//   alert('Correct!');
-// //   console.log('Correct, ' + name + '!');
-// } else {
-//   alert('Incorrect, sorry.');
-// //   console.log('Incorrect, ' + name + ' sorry.');
-// }
-
-// let questionFive = prompt('The last question is: Is my wife a flower farmer?').toLowerCase();
-// if (questionFive === '') {
-//   questionFive = prompt('Please answer yes or no.');
-// } else if (questionFive === 'yes') {
-//   alert('Correct!');
-// //   console.log('Correct, ' + name + '!');
-// } else {
-//   alert('Incorrect, sorry.');
-// //   console.log('Incorrect, ' + name + ' sorry.');
-// }
